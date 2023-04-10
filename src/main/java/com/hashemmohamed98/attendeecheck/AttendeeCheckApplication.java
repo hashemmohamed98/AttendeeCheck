c2ef9779264c8f690704db8c6fb05b2b3be72f37
hashemmohamed98 <hashem.mhashem98@gmail.com>
2023-04-10 07:44:55 +0200

    Committed

---
+++ src/main/java/com/hashemmohamed98/attendeecheck/AttendeeCheckApplication.java
@@ -0,0 +1,14 @@
+package com.hashemmohamed98.attendeecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class AttendeeCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendeeCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hashemmohamed98/attendeecheck/AttendeeCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hashemmohamed98.attendeecheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendeeCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
